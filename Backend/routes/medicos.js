a4b874cfdec6d89ff1aef53b122377a9f3bfec39
Keneth Fernando Vasquez Moran <81479808+kenethfv@users.noreply.github.com>
2021-05-08 14:50:02 -0500

    CRUDS a medicos, hospitales y usuarios, subida de archivos

---
+++ Backend/routes/medicos.js
@@ -0,0 +1,36 @@
+/*
+    Medicos
+    PATH: '/api/medicos'
+*/
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos')
+
+const { validarJWT } = require('../middlewares/validar-jwt');
+const { getMedicos, crearMedico, actualizarMedico, borrarMedico } = require('../controllers/medicos')
+
+const router = Router();
+
+router.get( '/' , validarJWT, getMedicos );
+
+router.post( '/',
+    [ 
+        validarJWT,
+        check('nombre', 'El nombre del Medico es necesario').not().isEmpty(),
+        check('hospital', 'El Id del Hospital debe ser valido es necesario').isMongoId(),
+        validarCampos    
+    ], 
+    crearMedico 
+ ); 
+
+ router.put( '/:id',
+ [ 
+    
+],
+actualizarMedico );
+
+ router.delete( '/:id',
+    borrarMedico 
+);
+
+module.exports = router; 
---
+++ Backend/routes/uploads.js
@@ -0,0 +1,21 @@
+/*
+    PATH: '/api/upload/:busqueda'
+*/
+const { Router } = require('express');
+const expressFileUpload = require('express-fileupload');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos')
+
+const { validarJWT } = require('../middlewares/validar-jwt');
+const { fileUpload, retornaImagen } = require('../controllers/uploads')
+
+const router = Router();
+
+// default options
+router.use( expressFileUpload() );
+
+router.put( '/:tipo/:id', validarJWT, fileUpload );
+
+router.get( '/:tipo/:foto', retornaImagen );
+
+module.exports = router; 
